0bc8576b00c16566e8aded64bc86dbc0f382d6f8
Fabian Sierra <frsierrag@gmail.com>
2020-08-18 00:16:20 -0500

    Modify params methods create and open Roulette

--- app/endpoints/createRoulette/createRouletteOperations.js
+++ app/endpoints/createRoulette/createRouletteOperations.js
@@ -1,8 +1,8 @@
 let firebase = require('../../common/firebase');
 exports.createRoulette = function (res, req) {
     let dataCreate = {};
-    dataCreate.id = Date.now().toString();
-    dataCreate.status = true;
+    dataCreate.idRoulette = Date.now().toString();
+    dataCreate.statusRoulette = true;
     console.log(dataCreate);
     firebase.ref('dataRoulette').push(dataCreate);     
     return res.res.status(200).send(dataCreate);
--- app/endpoints/openRoulette/openRouletteOperations.js
+++ app/endpoints/openRoulette/openRouletteOperations.js
@@ -1,11 +1,12 @@
 let firebase = require('../../common/firebase');
 exports.openRoulette = function (res, req) {
-    let openStatus = false;
+    let openStatus = {};
+    openStatus.statusOperation = false;
     firebase.ref('dataRoulette').once('value', (snapchot) => {
         const data = snapchot.val();
         for (let elementData in data) {
-            if (req.req.body.id == data[elementData].id) {
-                openStatus = data[elementData].status;
+            if (req.req.body.idRoulette == data[elementData].idRoulette) {
+                openStatus.statusOperation = data[elementData].status;
                 return res.res.status(200).send(openStatus); 
             }
         } 
